922256228cc4d470a80777ed97aad8c6d813bcff
Luiz Rodolfo Scudeller <rodolfoscudeller@gmail.com>
2024-01-04 18:07:43 -0300

    fix: dialect

--- src/config/config.js
+++ src/config/config.js
@@ -7,6 +7,6 @@ module.exports = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    dialect: process.env.DB_DIALECT,
+    dialect: 'mysql',
   },
 };
