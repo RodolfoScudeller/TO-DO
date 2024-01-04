243a6a8a79e86ba11d586c9d09ee761aafa64d3d
Luiz Rodolfo Scudeller <rodolfoscudeller@gmail.com>
2024-01-04 18:24:19 -0300

    feat: adding connection with url mysql

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -5,4 +5,15 @@ const sequelize = new Sequelize(process.env.MYSQL_URL, {
   logging: false, // Defina como true para ver as consultas SQL no console
 });
 
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Conexão bem-sucedida.');
+  } catch (error) {
+    console.error('Erro ao conectar ao banco de dados:', error);
+  }
+}
+
+testConnection();
+
 module.exports = sequelize;
