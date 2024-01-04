f316a74b203a3fc4d6864d1fef25dfa7d5f7ea38
Luiz Rodolfo Scudeller <rodolfoscudeller@gmail.com>
2024-01-04 18:06:24 -0300

    deleting mock config

--- src/config/config.js
+++ src/config/config.js
@@ -3,10 +3,10 @@ module.exports = {
     port: process.env.PORT || 3000,
   },
   database: {
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'a1b2c3',
-    database: process.env.DB_DATABASE || 'to_do_list',
-    dialect: process.env.DB_DIALECT || 'mysql',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    dialect: process.env.DB_DIALECT,
   },
 };
