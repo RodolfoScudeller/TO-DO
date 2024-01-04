db504f24f4684c6e8c54208c09d905e260b0d44f
Luiz Rodolfo Scudeller <rodolfoscudeller@gmail.com>
2024-01-04 18:16:13 -0300

    feat: adding connection with url mysql

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,14 +1,8 @@
 const Sequelize = require('sequelize');
 const config = require('./config');
 
-const sequelize = new Sequelize(
-  config.database.database,
-  config.database.user,
-  config.database.password,
-  {
-    host: config.database.host,
-    dialect: config.database.dialect,
-  },
-);
+const sequelize = new Sequelize(process.env.MYSQL_URL, {
+  logging: false, // Defina como true para ver as consultas SQL no console
+});
 
 module.exports = sequelize;
